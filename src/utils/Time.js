e308dde44659d3cca2a02b697b3957431bf5deee
Tin La <tinla14@gmail.com>
2020-10-17 22:07:49 -0700

    Update ux messages

--- src/utils/Time.js
+++ src/utils/Time.js
@@ -1,5 +1,8 @@
+import moment from 'moment';
 import { Timestamp } from '../db/firestore';
 
 export const createTimeStamp = () => {
     return Timestamp.now().toMillis().toString();
 }
+
+export const formatTimeAgo = timestamp => moment(parseInt(timestamp, 10)).fromNow();
