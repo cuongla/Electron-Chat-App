3f13600dac3efc2b9a8541296f956885efb8258e
Tin La <tinla14@gmail.com>
2020-10-17 21:16:18 -0700

    Fixed connection

--- src/actions/connection.js
+++ src/actions/connection.js
@@ -1,8 +1,8 @@
 import * as api from '../api/connection';
-import { 
-    CONNECTION_USER_STATUS_CHANGE
-} from '../actions/types';
+import { CONNECTION_USER_STATUS_CHANGED} from '../actions/types';
 
-export const checkUserConnection = () => dispatch => api.onConnectionChanged((isConnected) => {
-    dispatch({type: CONNECTION_USER_STATUS_CHANGE})
-})
+export const checkUserConnection = uid => dispatch =>
+  api.onConnectionChanged((isConnected) => {
+    api.setUserOnlineStatus(uid, isConnected)
+    dispatch({type: CONNECTION_USER_STATUS_CHANGED})
+  })
--- src/api/connection.js
+++ src/api/connection.js
@@ -1,5 +1,17 @@
-import firebbase from 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/database';
+import db from '../db/firestore';
+
+const getOnlineStatus = isOnline => ({
+    state: isOnline ? 'online' : 'offline',
+    lastChanged: firebase.firestore.FieldValue.serverTimestamp()
+})
+
+export const setUserOnlineStatus = (uid, isOnline) => {
+    const userRef = db.doc(`/profiles/${uid}`);
+    const updateData = getOnlineStatus(isOnline);
+    return userRef.update(updateData)
+}
 
 export const onConnectionChanged = onConnection =>
     firebase
