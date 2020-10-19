6a1bffc086278cdde4058f359025aa48249bc345
Tin La <tinla14@gmail.com>
2020-10-18 22:26:06 -0700

    Added multiple windows, dock icons, and tray menu

--- main.js
+++ main.js
@@ -2,13 +2,34 @@ const { app, BrowserWindow, ipcMain, Notification, Menu, Tray } = require('elect
 const path = require('path');
 const isDev = !app.isPackaged;
 
-// Main Process
-function createWindow() {
+const dockIcon = path.join(__dirname, 'assets', 'images', 'react_app_logo.png');
+const trayIcon = path.join(__dirname, 'assets', 'images', 'react_icon.png');
+
+
+
+function createSecondWindow() {
+    // Browser Window <- Renderer Process
     const win = new BrowserWindow({
         width: 1200,
-        height: 1000,
+        height: 800,
         backgroundColor: '#6e707e',
-        show: true,
+        webPreferences: {
+            nodeIntegration: false,
+            worldSafeExecuteJavaScript: true,
+            contextIsolation: true,
+        }
+    })
+
+    win.loadFile('second.html')
+}
+
+// Main Process
+function createWindow() {
+    // Browser Window <- Renderer Process
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        backgroundColor: '#6e707e',
         webPreferences: {
             nodeIntegration: false,
             worldSafeExecuteJavaScript: true,
@@ -19,7 +40,6 @@ function createWindow() {
 
     win.loadFile('index.html')
     isDev && win.webContents.openDevTools();
-    return win;
 }
 
 // reload electron in dev mode
@@ -29,28 +49,39 @@ if (isDev) {
     })
 }
 
+if (process.platform === 'darwin') {
+    app.dock.setIcon(dockIcon);
+}
+
 // create browser windows when Electron has finished initialization
+let tray = null;
 app.whenReady()
     .then(() => {
-        const template = require('./src/utils/Menu').createTemplate(app);
+        const template = require('./utils/Menu').createTemplate(app);
         const menu = Menu.buildFromTemplate(template);
         Menu.setApplicationMenu(menu);
+
+        tray = new Tray(trayIcon);
+        tray.setContextMenu(menu);
+        
         createWindow();
+        createSecondWindow();
     });
 
 ipcMain.on('notify', (_, message) => {
-    new Notification({ title: 'Notification', body: message })
-});
+    new Notification({ title: 'Notification', body: message }).show();
+})
 
 ipcMain.on('app-quit', () => {
     app.quit();
 })
 
+
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
     }
-});
+})
 
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
